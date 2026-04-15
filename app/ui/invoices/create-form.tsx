aa6eecdc7aa7e359a5162f71b72e3ff41899e0a8
huhejie <1933347443@qq.com>
2026-04-15 21:03:42 +0800

    feat: implement action state management in invoice and login forms

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -9,10 +9,10 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice, type State } from '@/app/lib/action';
-import { useFormState } from 'react-dom';
+import { useActionState } from 'react';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
-  const [state, dispatch] = useFormState(createInvoice, initialState);
+  const [state, dispatch] = useActionState(createInvoice, initialState);
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
